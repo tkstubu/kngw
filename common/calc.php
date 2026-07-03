7cd33b6b572acfc3ac22f8c34d2ee94f31369f47
Takashi MIYAKE <agt254km>
2026-07-04 02:52:21 +0900

    参加率の上限を100、達成率の上限を150に抑制するように修正

--- common/calc.php
+++ common/calc.php
@@ -593,13 +593,17 @@ function getCalcCampeignPoint($fiscal_year, $campaign, $item) {
     // 2026年度のLOにおけるサマーキャンペーン表彰は生産性得点、参加率得点、達成率得点に無条件で100点を加算する
     if ($fiscal_year == 2026 && $item === 'LO' && $campaign === 'summer') {
         $productPoint += 100;
-        $enterablePoint += 100;
+        $enterablePoint = 100;
         $reachPoint += 100;
+        if ($reachPoint > config::REACH_POINT_MAX) {
+		    $reachPoint = config::REACH_POINT_MAX;
+        }
+	}
     }
 
     // 2026年度のLHにおいては、サマーキャンペーン、秋キャンペーン、春キャンペーンのすべてにおいて、参加率得点を100点加算する
     if ($fiscal_year == 2026 && $item === 'LH' && in_array($campaign, ['summer', 'autumn', 'spring'])) {
-        $enterablePoint += 100;
+        $enterablePoint = 100;
     }
     
     // 計算に使用した数値
